2a1129acf028b2c81b43ef8ff402e7881d2676df
Justine Hoguet <justinehoguet@gmail.com>
2021-11-08 19:41:46 +0100

    New pizza

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,9 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
-    }
+    },
+    'my_pizza': {
+        'ingredients': {'garlic': 3, 'parmesan': 1, 'onion': 3},
+        'price': 11,
+}
 }
